dda0c01f9a898545fd5a7e45f6492f3201790f38
Moajjem Hossain <moajjem441@gmail.com>
2026-09-17 08:19:30 +0600

    server file added

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send("Hello from the server side (Prisma + PostgreSQL)");
 });
